9bf4c120359307fe8af34459e788236cceea2cb3
KingArtos <arturos.zica@gmail.com>
2018-06-28 11:54:58 -0300

    interface to poker hands

---
+++ lib/psycho-poker/poker_hand.rb
@@ -0,0 +1,18 @@
+class PokerHand
+  CARDS_LENGTH = 5
+
+  def self.check(cards = [])
+    raise ArgumentError.new("invalid cards size to #{self.class.name}, expect #{CARDS_LENGTH} and got #{cards.length}") if cards.length != CARDS_LENGTH
+    condition(cards) ? calc_points(cards) : 0
+  end
+
+  def self.calc_points(cards)
+    cards.reduce(0) do |acc, card|
+      acc += (CardPoints.points_with_special(card.value) * HandConfig.points_by_class(self))
+    end
+  end
+
+  def self.condition(cards = [])
+    raise NotImplementedError
+  end
+end
