2eaa0ff2bee8670310c8d62fb569ca498df909cc
KingArtos <arturos.zica@gmail.com>
2018-06-28 11:50:45 -0300

    initialize cards

--- lib/psycho-poker.rb
+++ lib/psycho-poker.rb
@@ -1,2 +1,7 @@
 class PsychoPoker
+  Dir["/lib/**/*.rb"].each {|file| load file }
+
+  def intialize(argument)
+    @cards = argument.split(' ').map { |possible_card| Card.new(possible_card) }
+  end
 end
